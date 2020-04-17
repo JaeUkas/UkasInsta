d493ba9639ef6f0ebf397fa3529c5d381c44dd50
JaeUkas <anytime96@naver.com>
2020-04-17 15:12:51 +0900

    INIT

---
+++ part_android/app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.instagramclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance();
+        email_login_button.setOnClickListener{
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            email_edittext.text.toString(),
+            password_edittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Creating a user account
+                    moveMainPage(task.result!!.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    //error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                } else {
+                    //Login
+                    signinEmail()
+                }
+            }
+    }
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(
+            email_edittext.text.toString(),
+            password_edittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                    moveMainPage(task.result!!.user)
+                } else {
+                    //error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
+
