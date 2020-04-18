890b29f378b6f977594f8efe705a53a273c88140
JaeUkas <anytime96@naver.com>
2020-04-19 02:39:12 +0900

    네비게이션 바 구현

--- part_android/app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ part_android/app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.example.instagramclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagramclone.navigation.AlarmFragment
+import com.example.instagramclone.navigation.DetailViewFragment
+import com.example.instagramclone.navigation.GridFragment
+import com.example.instagramclone.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment)
+                    .commit()
+                return true
+            }
+        }
+        return false
     }
 }
---
+++ part_android/app/src/main/java/com/example/instagramclone/navigation/DetailViewFragment.kt
@@ -0,0 +1,20 @@
+package com.example.instagramclone.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagramclone.R
+
+class DetailViewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+        return view
+    }
+}
+
