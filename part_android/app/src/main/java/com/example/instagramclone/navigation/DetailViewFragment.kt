cea2f5c0c5b9f5e6f8f1034a93f51142e38b999d
JaeUkas <anytime96@naver.com>
2020-04-27 14:33:51 +0900

    Add commentPage

--- part_android/app/src/main/java/com/example/instagramclone/navigation/DetailViewFragment.kt
+++ part_android/app/src/main/java/com/example/instagramclone/navigation/DetailViewFragment.kt
@@ -1,5 +1,6 @@
 package com.example.instagramclone.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -107,8 +108,12 @@ class DetailViewFragment : Fragment() {
                 fragment.arguments = bundle
                 activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_content,fragment)?.commit()
             }
+            viewHolder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                startActivity(intent)
+            }
         }
-
 
 
         fun favoriteEvent(position: Int) {
